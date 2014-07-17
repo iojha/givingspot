6aa621040482077d9a07a96ac06aa844201f3cb5
iojha <isha.ojha@gmail.com>
2014-07-17 16:57:13 -0600

    new layout

--- js/app.js
+++ js/app.js
@@ -1,3 +1,43 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
-$(document).foundation();
+$(document).ready( function() {
+  $(document).foundation();
+
+ var campaigns = [{name: "Maiti Nepal", organization: "Maiti Nepal", description: "helping young nepali girls find their way back into society", image: "img/fund1.jpg"},
+                  {name: "Aids Project Africa", organization: "Maiti Nepal", description: "helping young nepali girls find their way back into society", image: "img/fund2.jpg"},
+                  {name: "India Wells", organization: "Maiti Nepal", description: "helping young nepali girls find their way back into society", image: "img/fund3.jpg"},
+                  {name: "World Children Fund", organization: "Maiti Nepal", description: "helping young nepali girls find their way back into society", image: "img/fund4.jpg"},
+                  {name: "World Children Fund", organization: "Maiti Nepal", description: "helping young nepali girls find their way back into society", image: "img/fund4.jpg"},
+                  {name: "World Children Fund", organization: "Maiti Nepal", description: "helping young nepali girls find their way back into society", image: "img/fund4.jpg"},
+
+                  
+  ];
+
+  var output = "<div class=\"row\">";
+  var numCols = 3;
+
+  $.each(campaigns, function(index, campaign) {
+    output += "<div class=\"large-4 small-6 columns\"> <img src=\"" + campaign.image +
+              "\"> <div class=\"panel\"> <h5>" + campaign.name +
+              "</h5> <h6 class=\"subheader\">" + campaign.organization + "</h6> </div> </div> "
+
+    // close and open new row when filled
+
+    if (index % numCols === numCols - 1) {
+      output += " </div><div class=\"row\"> "
+    }
+
+    // if this is the last campaign, add empty divs to fix formatting
+
+    if (index == campaigns.length - 1) {
+      for (var c = 0; c < numCols - 1 - index % numCols; c++) {
+        output += "<div class=\"large-4 small-6 columns\"></div>"
+      }
+    }
+
+  });
+
+  output += "</div>"
+
+  $(".campaign-listing").html(output);
+});
