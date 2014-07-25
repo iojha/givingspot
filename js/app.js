3c59a73a18fdcee1a05eea21a133a47d070ae499
iojha <isha.ojha@gmail.com>
2014-07-25 16:28:25 -0600

    updating page to latest

--- js/app.js
+++ js/app.js
@@ -3,25 +3,23 @@
 $(document).ready( function() {
   $(document).foundation();
 
- var campaigns = [{name: "Maiti Nepal", organization: "Maiti Nepal", description: "helping young nepali girls find their way back into society", image: "img/fund1.jpg"},
-                  {name: "Aids Project Africa", organization: "Maiti Nepal", description: "helping young nepali girls find their way back into society", image: "img/fund2.jpg"},
-                  {name: "India Wells", organization: "Maiti Nepal", description: "helping young nepali girls find their way back into society", image: "img/fund3.jpg"},
-                  {name: "World Children Fund", organization: "Maiti Nepal", description: "helping young nepali girls find their way back into society", image: "img/fund4.jpg"},
-                  {name: "World Children Fund", organization: "Maiti Nepal", description: "helping young nepali girls find their way back into society", image: "img/fund4.jpg"},
-                  {name: "World Children Fund", organization: "Maiti Nepal", description: "helping young nepali girls find their way back into society", image: "img/fund4.jpg"},
-
-                  
-  ];
+ var campaigns = [{name: "Maiti Nepal", organization: "Maiti Nepal", description: "helping young nepali girls find their way back into society", image: "img/fund1.jpg", targ: "campaign_page.html"},
+                  {name: "Aids Project Africa", organization: "Maiti Nepal", description: "helping young nepali girls find their way back into society", image: "img/fund2.jpg", targ: "campaign_page.html"},
+                  {name: "India Wells", organization: "Maiti Nepal", description: "helping young nepali girls find their way back into society", image: "img/fund3.jpg", targ: "campaign_page.html"},
+                  {name: "World Children Fund", organization: "Maiti Nepal", description: "helping young nepali girls find their way back into society", image: "img/fund4.jpg", targ: "campaign_page.html"},
+                  {name: "World Children Fund", organization: "Maiti Nepal", description: "helping young nepali girls find their way back into society", image: "img/fund4.jpg", targ: "campaign_page.html"},
+                  {name: "World Children Fund", organization: "Maiti Nepal", description: "helping young nepali girls find their way back into society", image: "img/fund4.jpg", targ: "campaign_page.html"},];
+        
 
   var output = "<div class=\"row\">";
   var numCols = 3;
 
   $.each(campaigns, function(index, campaign) {
     output += "<div class=\"large-4 small-6 columns\"> <img src=\"" + campaign.image +
-              "\"> <div class=\"panel\"> <h5>" + campaign.name +
-              "</h5> <h6 class=\"subheader\">" + campaign.organization + "</h6> </div> </div> "
+              "\"> <a href=\"" + campaign.targ + "\"> <div class=\"panel\"> <h5>" + campaign.name +
+              "</h5> <h6 class=\"subheader\">" + campaign.organization + "</h6> </a> </div> </div> "
 
-    // close and open new row when filled
+      // close and open new row when filled
 
     if (index % numCols === numCols - 1) {
       output += " </div><div class=\"row\"> "
@@ -42,7 +40,6 @@ $(document).ready( function() {
   $(".campaign-listing").html(output);
 });
 
-//Campaign page
 
 
 
