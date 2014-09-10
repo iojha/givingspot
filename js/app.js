2397c485526fda7330a6b28a2c939e2db157f9cf
iojha <isha.ojha@gmail.com>
2014-09-10 10:37:56 -0600

    Add new photos to index

--- js/app.js
+++ js/app.js
@@ -3,12 +3,12 @@
 $(document).ready( function() {
   $(document).foundation();
 
- var campaigns = [{name: "Maiti Nepal", organization: "Maiti Nepal", description: "helping young nepali girls find their way back into society", image: "img/fund1.jpg", targ: "campaign_page.html"},
-                  {name: "Aids Project Africa", organization: "Maiti Nepal", description: "helping young nepali girls find their way back into society", image: "img/fund2.jpg", targ: "campaign_page.html"},
-                  {name: "India Wells", organization: "Maiti Nepal", description: "helping young nepali girls find their way back into society", image: "img/fund3.jpg", targ: "campaign_page.html"},
-                  {name: "World Children Fund", organization: "Maiti Nepal", description: "helping young nepali girls find their way back into society", image: "img/fund4.jpg", targ: "campaign_page.html"},
-                  {name: "World Children Fund", organization: "Maiti Nepal", description: "helping young nepali girls find their way back into society", image: "img/fund4.jpg", targ: "campaign_page.html"},
-                  {name: "World Children Fund", organization: "Maiti Nepal", description: "helping young nepali girls find their way back into society", image: "img/fund4.jpg", targ: "campaign_page.html"},];
+ var campaigns = [{name: "Maiti Nepal", organization: "Maiti Nepal", description: "Helping young nepali girls find their way back into society", image: "img/maitinepal.jpg", targ: "campaign_page.html"},
+                  {name: "Africa AIDS", organization: "AIDS Project Africa", description: "Supporting AIDS victims in Africa by providing care and medication", image: "img/fund2.jpg", targ: "campaign_page.html"},
+                  {name: "Wells for Good", organization: "Wells for Good", description: "Providing support by giving wells to villages going through drought in India", image: "img/fund3.jpg", targ: "campaign_page.html"},
+                  {name: "World Childrens' Fund", organization: "World Childrens' Fund", description: "Our organization gives underserved children in South East Asia the opportunity to attend school", image: "img/kids_03.jpg", targ: "campaign_page.html"},
+                  {name: "Bikes and Hikes", organization: "Bikes and Hikes Worldwide", description: "Encouraging developed nations to donate bikes to developing nations", image: "img/fund1.jpg", targ: "campaign_page.html"},
+                  {name: "Edutainment", organization: "Education for Good", description: "Providing education to children in South East Asia using latest technology and entertainment", image: "img/young-girl-nepal-school-5015_15.jpg", targ: "campaign_page.html"},];
         
 
   var output = "<div class=\"row\">";
